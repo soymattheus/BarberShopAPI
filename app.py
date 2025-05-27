47e48c563a198e6049ff2d8ff393a21a6be59cd3
Matheus Tavares <matheustavares1a@gmail.com>
2025-05-27 09:44:41 -0300

    config

--- app.py
+++ app.py
@@ -61,7 +61,8 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
 
 # pip freeze > requirements.txt
 # pip install -r requirements.txt
